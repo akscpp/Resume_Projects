e7200c0fc0753fb3a33f60d5b736968b0b4d7028
Akshit <129672950+akscpp@users.noreply.github.com>
2024-01-28 15:31:48 +0530

    Create config.py
    Contains important credentials for TempEye Project

---
+++ TempEye/config.py
@@ -0,0 +1,6 @@
+#Configurations for telegram_alert.py
+bolt_api_key = "XXXX"               
+device_id = "XXXX"                  
+telegram_chat_id = "@XXXX"          
+telegram_bot_id = "botXXXX"         
+threshold = 250                     
